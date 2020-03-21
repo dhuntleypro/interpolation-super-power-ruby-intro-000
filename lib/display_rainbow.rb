d935dcd1c26da4da121b8cbe42a813d4ca87385f
Darrien Huntley <dhuntleypro@gmail.com>
2020-03-21 21:32:31 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']).
+  puts "R: #{[0]}, O: #{[0]}, Y: #{[0]}, G: #{[0]}, B: #{[0]}, I: #{[0]}, V: #{[0]}"
